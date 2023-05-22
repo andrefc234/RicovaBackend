e39f3f72db8e4307bd23f79b9ba13c64a5cc8600
andrefc234 <andre.flamand1998@gmail.com>
2023-05-22 16:07:13 -0500

    first commit

---
+++ src/api/negotation/controllers/negotation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * negotation controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::negotation.negotation');
---
+++ src/api/negotation/routes/negotation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * negotation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::negotation.negotation');
